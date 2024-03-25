6981cd1d7c2e8130efb50af0c93d19b736c23124
Luis Salcedo <lsalcedo110@gmail.com>
2024-03-25 03:06:36 -0400

    meh

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String localResult = '0';
+  List<double> localNumbers = [];
+  double currentNumber = 0;
+
+  void acFunc() {
+    setState(() {
+      localResult = '0';
+      localNumbers.clear();
+      currentNumber = 0;
+    });
+  }
+
+  void add() {
+    localNumbers.add(double.parse(localResult));
+    setState(() {
+      currentNumber = 0;
+      localResult = '0';
+    });
+  }
+
+  void multiply() {
+    localNumbers.add(double.parse(localResult));
+    setState(() {
+      currentNumber = 0;
+      localResult = '0';
+    });
+  }
+
+  void calculateResult() {
+    if (localNumbers.isNotEmpty) {
+      localNumbers.add(double.parse(localResult));
+      double result = localNumbers.reduce((value, element) => value + element);
+      setState(() {
+        localResult = result.toString();
+        localNumbers.clear();
+        currentNumber = result;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Calculator",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
@@ -17,13 +62,24 @@ class HomeScreen extends StatelessWidget {
           const SizedBox(
             height: 80,
           ),
-          const Text(
-            "Text Here",
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                localResult,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                "Result goes here",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
           const SizedBox(
             height: 20,
@@ -32,45 +88,57 @@ class HomeScreen extends StatelessWidget {
             crossAxisCount: 4,
             shrinkWrap: true,
             children: [
-              calculatorCell('AC', Colors.grey[700], () {}),
+              calculatorCell('AC', Colors.grey[700], acFunc),
               calculatorCell('*/-', Colors.grey[700], () {}),
               calculatorCell('%', Colors.grey[700], () {}),
-              calculatorCell('/', Colors.orange, () {}),
-
-              calculatorCell('7', Colors.grey[500], () {}),
-              calculatorCell('8', Colors.grey[500], () {}),
-              calculatorCell('9', Colors.grey[500], () {}),
-              calculatorCell('X', Colors.orange, () {}),
-
-              calculatorCell('4', Colors.grey[500], () {}),
-              calculatorCell('5', Colors.grey[500], () {}),
-              calculatorCell('6', Colors.grey[500], () {}),
-              calculatorCell('-', Colors.orange, () {}),
-
-              calculatorCell('1', Colors.grey[500], () {}),
-              calculatorCell('2', Colors.grey[500], () {}),
-              calculatorCell('3', Colors.grey[500], () {}),
-              calculatorCell('+', Colors.orange, () {}),
-
-              calculatorCell('0', Colors.grey[500], () {}),
-              calculatorCell('.', Colors.grey[500], () {}),
-              calculatorCell('=', Colors.orange, () {}),
-
-              // calculatorCell("8", () {}),
-              // calculatorCell("9", () {}),
-              // calculatorCell("/", () {}),
-              // calculatorCell("4", () {}),
-              // calculatorCell("5", () {}),
-              // calculatorCell("6", () {}),
-              // calculatorCell("X", () {}),
-              // calculatorCell("1", () {}),
-              // calculatorCell("2", () {}),
-              // calculatorCell("3", () {}),
-              // calculatorCell("-", () {}),
-              // calculatorCell("0", () {}),
-              // calculatorCell(".", () {}),
-              // calculatorCell("=", () {}),
-              // calculatorCell("+", () {}),
+              calculatorCell('÷', Colors.orange, () {
+                add();
+              }),
+              calculatorCell('7', Colors.grey[500], () {
+                updateLocalResult('7');
+              }),
+              calculatorCell('8', Colors.grey[500], () {
+                updateLocalResult('8');
+              }),
+              calculatorCell('9', Colors.grey[500], () {
+                updateLocalResult('9');
+              }),
+              calculatorCell('x', Colors.orange, () {
+                multiply();
+              }),
+              calculatorCell('4', Colors.grey[500], () {
+                updateLocalResult('4');
+              }),
+              calculatorCell('5', Colors.grey[500], () {
+                updateLocalResult('5');
+              }),
+              calculatorCell('6', Colors.grey[500], () {
+                updateLocalResult('6');
+              }),
+              calculatorCell('-', Colors.orange, () {
+                updateLocalResult('-');
+              }),
+              calculatorCell('1', Colors.grey[500], () {
+                updateLocalResult('1');
+              }),
+              calculatorCell('2', Colors.grey[500], () {
+                updateLocalResult('2');
+              }),
+              calculatorCell('3', Colors.grey[500], () {
+                updateLocalResult('3');
+              }),
+              calculatorCell('+', Colors.orange, () {
+                add();
+              }),
+              calculatorCell('0', Colors.grey[500], () {
+                updateLocalResult('0');
+              }),
+              calculatorCell('.', Colors.grey[500], () {
+                updateLocalResult('.');
+              }),
+              calculatorCell('=', Colors.orange, () {
+                calculateResult();
+              }),
             ],
           ),
         ],
@@ -80,26 +148,38 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+  void updateLocalResult(String value) {
+    setState(() {
+      if (localResult == '0' && value != '.') {
+        localResult = value;
+      } else {
+        localResult += value;
+      }
+    });
+  }
 }
 
 Widget calculatorCell(String cellTitle, Color? cellColor, Function function,
     [Color? textColor = Colors.white]) {
   return Padding(
     padding: const EdgeInsets.all(8.0),
-    child: GestureDetector(
-      onTap: function(),
+    child: InkWell(
+      onTap: () => function(),
       child: Container(
         decoration: BoxDecoration(
           color: cellColor,
           borderRadius: BorderRadius.circular(20),
         ),
         child: Center(
-            child: Text(
-          cellTitle,
-          style: TextStyle(
-            color: textColor,
+          child: Text(
+            cellTitle,
+            style: TextStyle(
+              fontSize: 45,
+              color: textColor,
+            ),
           ),
-        )),
+        ),
       ),
     ),
   );
--- lib/utils/functionality.dart
+++ lib/utils/functionality.dart
@@ -1,12 +1,46 @@
+class Calculator {
+  double _result = 0;
 
+  double get result => _result;
 
-class Calculator{
-  // List numbers = [];
-  
+  void reset() {
+    _result = 0;
+  }
 
-  void addNumbers(List<int> numbers){
-    for(int i = 0; i < numbers.length; i++){
+  void addNumbers(List<double> numbers) {
+    for (var number in numbers) {
+      _result += number;
+    }
+  }
 
+  void subtractNumbers(List<double> numbers) {
+    if (numbers.isNotEmpty) {
+      _result = numbers.first;
+      for (int i = 1; i < numbers.length; i++) {
+        _result -= numbers[i];
+      }
+    }
+  }
+
+  void multiplyNumbers(List<double> numbers) {
+    _result = 1;
+    for (var number in numbers) {
+      _result *= number;
+    }
+  }
+
+  void divideNumbers(List<double> numbers) {
+    if (numbers.isNotEmpty) {
+      _result = numbers.first;
+      for (int i = 1; i < numbers.length; i++) {
+        if (numbers[i] != 0) {
+          _result /= numbers[i];
+        } else {
+          // Handle division by zero error
+          _result = double.infinity; // Set result to infinity
+          break; // Exit loop
+        }
+      }
     }
   }
 }
